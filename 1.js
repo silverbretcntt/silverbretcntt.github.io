ffd8db3e0df836d6a8c98ddb4fe49ba1d6c47a83
Phu Dep Trai <nguyenvanphucntt@gmail.com>
2018-03-21 00:32:57 +0700

    up giao dien web

---
+++ 1.js
@@ -0,0 +1,24 @@
+﻿$(function(){
+	$(window).scroll(function(){
+		vitrihientai = $('html, body').scrollTop();
+		if(vitrihientai > 337){
+			$('.navbar-fixed-top').addClass('nho');
+		}
+		 else if(vitrihientai <= 337){
+			$('.navbar-fixed-top').removeClass('nho');
+		}
+	});
+	$('.btn1').click(function(){
+		$('html, body').animate({scrollTop:$('#sanpham').offset().top});
+		return false;
+	})
+		$('.btn2').click(function(){
+		$('html, body').animate({scrollTop:$('#gioithieu').offset().top});
+		return false;
+	})
+		$('.btn3').click(function(){
+		$('html, body').animate({scrollTop:$('#lienhe').offset().top});
+		return false;
+	})
+
+});
